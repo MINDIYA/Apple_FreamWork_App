e69f4038b8a39858c4d513326298e383f246f89e
MINDIYA <mindiyamaitipe@gmail.com>
2025-11-16 16:16:05 +0530

    Initial Commit

---
+++ Apple_FreamWork_App/Apple_FreamWork_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FreamWork_AppApp.swift
+//  Apple_FreamWork_App
+//
+//  Created by Mindiya Lankaja Maitipe on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FreamWork_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
